f639fac0661dae0e55429192fd5abfc83228ed17
optimistic_shekhar <optimistic_shekhar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-08 06:19:37 +0000

    version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/shk-hide-title/tags/1.0.1@1194579 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: SHK Hide Title
+ * Plugin URI: http://shekharbhandari.com.np/shk_hide_title
+ * Description: A Plugin To disable title for pages in WordPress
+ * Version: 1.0.1
+ * Author: Shekhar Bhandari
+ * Author URI: http://shekharbhandari.com.np/
+ * Text Domain: shk-hide-title
+ * License: GPL2
+ */
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+include(__DIR__.'/hide_title.php');
+
+?>
